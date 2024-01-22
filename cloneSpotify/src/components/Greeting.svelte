84f004f3fb4c8523376f3c7fed3b508a4de5f2b8
Tomas Miranda <tomasmiranda98@hotmail.com>
2024-01-22 13:04:30 -0300

    feat: se establece base para reproductor

---
+++ cloneSpotify/src/components/Greeting.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+
+    const currentTime = Date()
+    const currentHour = new Date(currentTime).getHours()
+
+    let greeting = ''
+
+    if (currentHour < 12) {
+        greeting = 'Buenos dias!'
+    } else if (currentHour < 18) {
+        greeting = 'Buenas tardes!'
+    } else {
+        greeting = 'Buenas noches!'
+    }
+</script>
+
+<h1 class="text-4xl font-bold" >{greeting}</h1>
