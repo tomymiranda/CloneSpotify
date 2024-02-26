2d2215e209fe379a479a5600bd6047d13a8b0182
Tomas Miranda <tomasmiranda98@hotmail.com>
2024-02-26 12:44:04 -0300

    feat: se agrega funcionamiento de api propio

--- cloneSpotify/src/components/CardPlayButton.jsx
+++ cloneSpotify/src/components/CardPlayButton.jsx
@@ -2,20 +2,32 @@ import { Pause,Play } from "./Player"
 import { usePlayStore } from "../store/playStore"
 export const CardPlayButton = ({id}) => {
     const {currentMusic, 
-        isPlaying, 
-        setCurrentMusic, 
-        setIsPlaying } = usePlayStore(state => state)
+            isPlaying, 
+            setCurrentMusic, 
+            setIsPlaying } = usePlayStore(state => state)
+    
+    
+    const isPlayingPlaylist = isPlaying && currentMusic?.playlist.id === id
     
     const handleClick = () => {
-     setCurrentMusic({
-            playlist: {
-             id
-            }
-     })
-     setIsPlaying(!isPlaying)       
+        if(isPlayingPlaylist){
+            setIsPlaying(false)
+            return
+        }
+        // hacerlo despues con async await 
+        fetch(`/api/get-info-playlist.json?id=${id}`)
+        .then(response => response.json())
+        .then(data => {
+            const {songs, playlist} = data
+            setIsPlaying(true)
+            setCurrentMusic({songs, playlist, song: songs[0]})
+
+        })
+        
+        console.log("songs", songs)
+        console.log("playlist", playlist)
     }
 
-    const isPlayingPlaylist = isPlaying && currentMusic?.playlist.id === id
     
     return (
         <button onClick={handleClick} className="card-play-button rounded-full bg-green-500 p-4">
--- cloneSpotify/src/pages/api/get-info-playlist.json.js
+++ cloneSpotify/src/pages/api/get-info-playlist.json.js
@@ -1,3 +1,22 @@
-export async function get(){
-    
+import { allPlaylists, songs as allSongs } from "../../lib/data"
+
+export async function GET(req, res) {
+    console.log("antes de ejecutar")
+    const { url } = req
+    console.log("url", url)
+    const urlObject = new URL(url)
+    const id = urlObject.searchParams.get('id')
+    console.log("id", id)
+    //busco la playlist con la q se obtuvo el id
+    const playlist = allPlaylists.find(playlist => playlist.id === id)
+    const songs = allSongs.filter(song => song.albumId === playlist?.albumId)
+
+    console.log("playlist aPI", playlist)
+    console.log("songs API", songs)
+
+    return new Response(JSON.stringify({ playlist, songs }), {
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
 }
